bca77a86b7f188c85fe88591e4716e2740c3576f
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-10 09:34:51 +0000

    Pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Pascal's triangle
+"""
+def pascal_triangle(n):
+    """ Pascal's triangle """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(0, n - 1):
+        row = [1]
+        for j in range(len(triangle[-1]) - 1):
+            row.append(triangle[-1][j] + triangle[-1][j + 1])
+        row.append(1)
+        triangle.append(row)
+    return triangle
---
+++ README.md
@@ -0,0 +1,1 @@
+Practicing interview style questions
