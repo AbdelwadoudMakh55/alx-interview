993dce6478f5ba2d29003498e1ea9515b7824511
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-22 21:15:46 +0000

    Minimum operations solution

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Solution file for the minimum operations problem.
+"""
+
+
+def check_prime(n: int) -> bool:
+    """ Check if integer is prime """
+    i = 2
+    while i < n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+
+def minOperations(n: int) -> int:
+    """ Count number of minimum operations """
+    sum_prime_factors = []
+    i = 2
+    while n != 1:
+        if check_prime(i) and n % i == 0:
+            n = n // i
+            sum_prime_factors.append(i)
+        else:
+            i += 1
+
+    return sum(sum_prime_factors)
