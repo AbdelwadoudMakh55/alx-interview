57eaa91597de322b2f196bac85f487c9031d54bb
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-03 16:26:12 +0200

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,7 +11,7 @@ def check_format(line: str) -> bool:
     ip_adress = r'^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?){4}'
     date = r'\[\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}\.\d+\]'
     status_code = '200|301|400|401|403|404|405|500'
-    file_size = r'\b(0|[1-9]\d{0,2}|102[0-4])\b'
+    file_size = r'0|[1-9]\d{0,2}|102[0-4]'
     regex = f'{ip_adress} - {date} "GET /projects/260 HTTP/1.1"\
  {status_code} {file_size}$'
     if re.search(regex, line):
@@ -22,7 +22,7 @@ def check_format(line: str) -> bool:
 def extract_size_file_st_code(line: str) -> Tuple[int, int]:
     size = ""
     status_code = ""
-    i = -1
+    i = -2
     while line[i] != ' ':
         size += line[i]
         i -= 1
@@ -30,7 +30,9 @@ def extract_size_file_st_code(line: str) -> Tuple[int, int]:
     while line[i] != ' ':
         status_code += line[i]
         i -= 1
-    return int(size[::-1]), int(status_code[::-1])
+    size = int(size[::-1]) if size.isnumeric() else 0
+    status_code = int(status_code[::-1]) if status_code.isnumeric() else 0
+    return size, status_code
 
 
 if __name__ == '__main__':
@@ -43,7 +45,8 @@ if __name__ == '__main__':
                 i += 1
                 size, status_code = extract_size_file_st_code(line)
                 size_files += size
-                status[status_code] += 1
+                if status_code != 0:
+                    status[status_code] += 1
             if i == 10:
                 i = 0
                 print(f'File size: {size_files}')
