eea3c5078289171ced816527f21778b2d9fd578e
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-16 20:49:30 +0200

    Use different approach

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" This is the 100-nqueen module
+""" Solving the n queens challenge
 """
 import sys
 
 
 def check_int(string):
+    """ Check if the second argument is a valid integer """
     result = 1
     if string[0] == '0' and len(string) != 1:
         return 0
@@ -15,103 +16,49 @@ def check_int(string):
     return result
 
 
-def bound_fun(mat):
-    for i in range(len(mat)):
-        sum_row = 0
-        for j in range(len(mat)):
-            sum_row += mat[i][j]
-        if sum_row > 1:
-            return 0
+def nqueens(n):
+    """ Implementation of the solution for the n queens challenge """
+    all_results = []
+    result = [[0] * 2 for i in range(n)]
+    col = set()
+    posDiag = set()
+    negDiag = set()
 
-    for i in range(len(mat)):
-        sum_col = 0
-        for j in range(len(mat)):
-            sum_col += mat[j][i]
-        if sum_col > 1:
-            return 0
-    for i in range(len(mat)):
-        for j in range(len(mat)):
-            sum_diag2 = 0
-            sum_diag1 = 0
-            for k in range(len(mat) - j - i):
-                sum_diag1 += mat[i + k][j + k]
-                sum_diag2 += mat[len(mat) - 1 - i - k][j + k]
-            if sum_diag1 > 1 or sum_diag2 > 1:
-                return 0
-    return 1
+    def backtrack(r):
+        """ Recursive backtracking function """
+        if r == n:
+            copy_results = [row[:] for row in result]
+            all_results.append(copy_results)
+            return
 
-
-if len(sys.argv) == 1 or len(sys.argv) > 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-if not check_int(sys.argv[1]):
-    print("N must be a number")
-    sys.exit(1)
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-n = int(sys.argv[1])
+        for c in range(n):
+            if c in col or r + c in posDiag or r - c in negDiag:
+                continue
+            col.add(c)
+            posDiag.add(r + c)
+            negDiag.add(r - c)
+            result[r][0] = r
+            result[r][1] = c
+            backtrack(r + 1)
+            col.remove(c)
+            posDiag.remove(r + c)
+            negDiag.remove(r - c)
+            result[r][0] = 0
+            result[r][1] = 0
+    backtrack(0)
+    for row in all_results:
+        print(row)
 
 
-def nqueens(n, start):
-    s_matr = []
-    for i in range(n):
-        row1 = []
-        for j in range(2):
-            row1.append(0)
-        s_matr.append(row1)
-
-    q_track = {}
-    for i in range(n):
-        q_track[str(i + 1)] = 0
-
-    place = 0
-    board = []
-    for i in range(n):
-        row = []
-        for j in range(n):
-            row.append(0)
-        board.append(row)
-
-    i = 0
-    j = start
-    while place < n:
-        while j == n:
-            if q_track[str(i + 1)] == 0:
-                place -= 1
-                if i != 0:
-                    i -= 1
-                board[i][q_track[str(i + 1)]] = 0
-                j = q_track[str(i + 1)] + 1
-                q_track[str(i + 1)] = 0
-            else:
-                q_track[str(i + 1)] = 0
-                place -= 1
-                i -= 1
-                board[i][q_track[str(i + 1)]] = 0
-                j = q_track[str(i + 1)] + 1
-        while j < n:
-            board[i][j] = 1
-            if bound_fun(board) == 0:
-                board[i][j] = 0
-                j += 1
-            else:
-                place += 1
-                q_track[str(i + 1)] = j
-                i += 1
-                j = 0
-                break
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == 1:
-                s_matr[i][0] = i
-                s_matr[i][1] = j
-    print(s_matr)
-
-
-i = 0
-if int(sys.argv[1]) == 4 or int(sys.argv[1]) == 6:
-    i = 1
-while i < int(sys.argv[1]) - 1:
-    nqueens(n, i)
-    i += 1
+if __name__ == '__main__':
+    if len(sys.argv) == 1 or len(sys.argv) > 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if not check_int(sys.argv[1]):
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    n = int(sys.argv[1])
+    nqueens(n)
