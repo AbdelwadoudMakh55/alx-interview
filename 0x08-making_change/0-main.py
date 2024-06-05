5f4fc6e843c97b65785012fc2a2cdffa399ddec5
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-06-05 22:25:52 +0100

    Solve the problem using dynamic bottom up approach

--- 0x08-making_change/0-main.py
+++ 0x08-making_change/0-main.py
@@ -5,10 +5,6 @@ Main file for testing
 
 makeChange = __import__('0-making_change').makeChange
 
-print(makeChange([1, 5, 10, 25], 99))
-
-print(makeChange([2, 3, 5, 7], 27))
-
 print(makeChange([1, 2, 25], 37))
 
 print(makeChange([1256, 54, 48, 16, 102], 1453))
