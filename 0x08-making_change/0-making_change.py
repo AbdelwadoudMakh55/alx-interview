27d94f9279cc1eeba8c8a014e637b7dc533f493d
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-06-05 22:41:39 +0100

    Solve the problem using dynamic bottom up approach

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,11 +8,14 @@ def makeChange(coins, total):
     """Function for the solution"""
     if total <= 0:
         return 0
+    if total in coins:
+        return 1
     coins.sort()
     dp = [total + 1] * (total + 1)
     dp[0] = 0
     for i in range(1, total + 1):
         for c in coins:
-            if i - c >= 0:
-                dp[i] = min(dp[i], 1 + dp[i - c])
+            if c > i:
+                break
+            dp[i] = min(dp[i], 1 + dp[i - c])
     return dp[total] if dp[total] != total + 1 else -1
