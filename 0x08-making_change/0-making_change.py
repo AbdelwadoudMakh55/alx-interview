25b239ccb89b7975b4e18e7ba1990b44d4fa97a3
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-06-05 22:16:44 +0100

    Solve the problem using dynamic bottom up approach

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,20 +6,11 @@ Implement a solution for making change problem
 
 def makeChange(coins, total):
     """Function for the solution"""
-    if total <= 0:
-        return -1
-    coins_count = 0
-    sum = 0
-    while sum < total and len(coins) > 0:
-        if max(coins) + sum < total:
-            sum += max(coins)
-            coins_count += 1
-        elif max(coins) + sum == total:
-            sum += max(coins)
-            coins_count += 1
-        else:
-            if len(coins) > 0:
-                coins.pop(coins.index(max(coins)))
-    if sum != total:
-        return -1
-    return coins_count
+    coins.sort()
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+    for i in range(1, total + 1):
+        for c in coins:
+            if i - c >= 0:
+                dp[i] = min(dp[i], 1 + dp[i - c])
+    return dp[total] if dp[total] != total + 1 else -1
